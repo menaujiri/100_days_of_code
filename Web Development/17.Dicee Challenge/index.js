bd40133ec57e0848293685bf8eed8da8c9de5668
menaujiri <ujirimena@gmail.com>
2024-07-06 07:27:22 +0100

    Random dice

--- Web Development/17.Dicee Challenge/index.js
+++ Web Development/17.Dicee Challenge/index.js
@@ -1,3 +1,14 @@
-var randomNumber1 = math.floor(math.ramndom() * 6 + 1);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
 
-document
+var img1 = document.querySelector(".img1");
+img1.setAttribute("src", randomImageSource)
+
+var randomNumber2 = math.floor(Math.random() *6) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var img2 = document.querySelector(".img2");
+img2.setAttribute("src", randomImageSource2)
+
