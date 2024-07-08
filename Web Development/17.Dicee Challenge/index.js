4fbc0612fa5ac4d15c1a7f4d6602dcf987ad88e0
menaujiri <ujirimena@gmail.com>
2024-07-08 05:24:51 +0100

    update

--- Web Development/17.Dicee Challenge/index.js
+++ Web Development/17.Dicee Challenge/index.js
@@ -3,12 +3,12 @@ var randomDiceImage = "dice" + randomNumber1 + ".png";
 var randomImageSource = "images/" + randomDiceImage;
 
 var img1 = document.querySelector(".img1");
-img1.setAttribute("src", randomImageSource)
+img1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = math.floor(Math.random() *6) + 1;
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 var randomImageSource2 = "images/" + randomDiceImage2;
 
 var img2 = document.querySelector(".img2");
-img2.setAttribute("src", randomImageSource2)
+img2.setAttribute("src", randomImageSource2);
 
