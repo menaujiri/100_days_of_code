306503d51d443121f110a8fe5e0ecf0128fe79c7
menaujiri <ujirimena@gmail.com>
2024-07-09 04:25:24 +0100

    update

--- Web Development/18.Drum Kit/index.js
+++ Web Development/18.Drum Kit/index.js
@@ -1,17 +1,30 @@
 
 var numberOfDrumKit = document.querySelectorAll(".drum").length
 
-var audio = new Audio("sounds/tom-1.mp3");
-
 for(var i = 0; i<numberOfDrumKit; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        this.style.color = "white"
+        
+        var buttonInnerHTML = this.innerHTML;
+        
+        switch (buttonInnerHTML)) {
+            case "w": 
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                Audio.play()
+                break;
+
+            
+        
+            default: console.log(buttonInnerHTML)
+                break;
+        }
+
+
     });
 }
 
 
 
-//audio.play();
+//audio.play();kl
 
 
 
