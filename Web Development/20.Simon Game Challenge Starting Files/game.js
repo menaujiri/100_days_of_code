ffc149180062eacd0e653d60d6e1003c6655f50d
menaujiri <ujirimena@gmail.com>
2024-07-12 07:27:25 +0100

    game js

---
+++ Web Development/20.Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,13 @@
+
+
+var bottonColours = ["red", "blue", "green", "yellow"];
+
+gamePattern = [];
+
+var randomNumber = Math.floor(Math.random() *4);
+
+
+var randomChosenColour = bottonColours[randomNumber];
+
+gamePattern.push(randomChosenColour);
+
