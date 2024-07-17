f84192162d82e615c3a971fda7c14a56e330a99f
menaujiri <ujirimena@gmail.com>
2024-07-17 05:29:17 +0100

    url

--- Web Development/Backend/2.4 QR Code Project/index.js
+++ Web Development/Backend/2.4 QR Code Project/index.js
@@ -1,6 +1,8 @@
 
 //1. Use the inquirer npm package to get user input.
 import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
 
 inquirer
   .prompt([{
@@ -8,7 +10,15 @@ inquirer
     name: "URL"
   }])
   .then((answers) => {
-   console.log(answers);
+   const url = answers.URL;
+   var qr_svg = qr.image(url);
+   qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+   fs.writeFile('URL.txt', url, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  }); 
+
   })
   .catch((error) => {
     if (error.isTtyError) {
